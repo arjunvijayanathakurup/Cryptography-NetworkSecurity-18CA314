447751f60836ebbc7f88455af0cc40c2c7a21d0c
arjunvijayanathakurup <arjunvijayanathakurup@gmail.com>
2020-02-14 09:46:10 +0530

    updated extended euclidean

--- extended_euclidean.py
+++ extended_euclidean.py
@@ -1,35 +1,22 @@
-import math
+def ext_euc(a, b):
+    s = 0
+    old_s = 1
+    t = 1
+    old_t = 0
+    r = a 
+    old_r = b  
+    while b != 0:
+        q = a // b
+        (a, b) = (b, a % b)
+        (s, old_s) = ((old_s - (q * s)), s)
+        (t, old_t) = ((old_s - (q * t)), t)
+    if old_s < 0:
+        old_s += old_r
+    if old_t < 0:
+        old_t += r
+    return old_s
 
-def gcd(r, old_r, s, old_s, t, old_t):
-    while(r != 0):
-        q = math.floor(old_r / r) 
-
-        temp_oldr = old_r
-        old_r = r
-        tempr = r
-        r = temp_oldr - q * tempr 
-
-        temp_olds = old_s
-        old_s = s
-        temps = s
-        s = temp_olds - q * temps
-
-        temp_oldt = old_t
-        old_t = t
-        tempt = t
-        t = temp_oldt - q * tempt
-        print(r, old_r)
-        
-        if r == 0:
-            print("Multiplicative inverse is " + str(old_r))
-        val = gcd(r, old_r, s, old_s, t, old_t)
-        return val
-
-s = 0
-old_s = 1
-t = 1
-old_t = 0
 print("gcd(a, b) => Enter a and b: ")
 a = int(input())
 b = int(input())
-gcd(a, b, s, old_s, t, old_t)
+print(ext_euc(a, b))
